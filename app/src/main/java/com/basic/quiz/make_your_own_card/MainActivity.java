87226f4dbf31dd694fac33a5ff7a9131311802aa
SakshamKhanuja <ssk@outlook.ie>
2021-11-15 11:13:05 +0530

    Initial Commit

---
+++ app/src/main/java/com/basic/quiz/make_your_own_card/MainActivity.java
@@ -0,0 +1,14 @@
+package com.basic.quiz.make_your_own_card;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
